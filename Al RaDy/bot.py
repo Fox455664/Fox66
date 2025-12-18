a724b8d545a6135d3c2caeeb0882b31d6849e349
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2025-12-18 09:45:21 +0200

    Update bot.py

--- Al RaDy/bot.py
+++ Al RaDy/bot.py
@@ -2,8 +2,8 @@ from pyrogram import Client, idle
 from pyromod import listen
 from casery import caes, casery, group, source, photosource, caserid, ch, bot_token, bot_token2
 
-bot = Client("CAR", api_id=8186557, api_hash="efd77b34c69c164ce158037ff5a0d117", bot_token=bot_token, plugins=dict(root="CASER"))
-lolo = Client("hossam", api_id=8186557, api_hash="efd77b34c69c164ce158037ff5a0d117", session_string=bot_token2)    
+bot = Client("CAR", api_id=24722068, api_hash="72feca3ed88891eeff3852e20817cdca", bot_token=bot_token, plugins=dict(root="CASER"))
+lolo = Client("hossam", api_id=24722068, api_hash="72feca3ed88891eeff3852e20817cdca", session_string=bot_token2)    
 
 DEVS = caes
 DEVSs = []
