bc38a06211a11120e72873c71a43f11e71601360
اسمك <@example.com>
2025-12-18 09:40:01 +0200

    تم تحديث الملفات وفك الضغط من تيرمكس

---
+++ Al RaDy/casery.py
@@ -0,0 +1,22 @@
+##مطورين السورس
+caes = ["AlRa_dy","P1_w5","C_HRr"]
+casery = "AlRa_dy"
+##ايدي مطور السورس الاساسي
+caserid = 5184436120
+##اسم البوتات بالعربى
+OWNER = "راضى"
+##اسمك&الميوزك
+muusiic = "Al RaDy MuSiC" 
+##اسم السورس ع صوره التشغيل
+suorce = "SoUrCe Al RaDy" 
+##قناه السورس
+source = "https://t.me/HK_CI" 
+ch = "HK_CI" 
+##جروب السورس
+group = "https://t.me/B_M_S_R" 
+##لوجو السورس(صوره السورس) 
+photosource = "https://graph.org/file/5f835c5809ebce4f30e4d.jpg"
+##توكن الصانع
+bot_token="توكن"
+##جلسه الاشتراك
+bot_token2="جلسه"
