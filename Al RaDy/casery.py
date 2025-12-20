8e31303d2227e4f2df5b68a8fe06985e1ac4334c
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2025-12-20 06:45:21 +0200

    Update casery.py

--- Al RaDy/casery.py
+++ Al RaDy/casery.py
@@ -1,10 +1,10 @@
 ##مطورين السورس
-caes = ["AlRa_dy","P1_w5","C_HRr"]
+caes = ["f_o_x_351","P1_w5","C_HRr"]
 casery = "AlRa_dy"
 ##ايدي مطور السورس الاساسي
-caserid = 5184436120
+caserid = 7669264153
 ##اسم البوتات بالعربى
-OWNER = "راضى"
+OWNER = "فوكس"
 ##اسمك&الميوزك
 muusiic = "Al RaDy MuSiC" 
 ##اسم السورس ع صوره التشغيل
@@ -17,6 +17,7 @@ group = "https://t.me/B_M_S_R"
 ##لوجو السورس(صوره السورس) 
 photosource = "https://graph.org/file/5f835c5809ebce4f30e4d.jpg"
 ##توكن الصانع
-bot_token="توكن"
+bot_token="8550161677:AAFAMOORMi_TVRg5uvbObV9GAqext4HdW10"
 ##جلسه الاشتراك
-bot_token2="جلسه"
+
+bot_token2="BAF5OpQAGRbP2KrVZiJGqhgbLnYFXLmSriIrIjPXaOfm7MrMZ9rOX1yVH8T_eMCrmJPOYfGB3jtKQ8X9weHJJ-D1Enrwncbn3oHA0FbSbR1SXATrTtH-F_l7ne-vQwAwyPCvoLVJt3PZhQwrPNGFngiIK0IfxVQ3SVmDHLFNQVsBpZDEf9v-fwwJ_VMPqH0uVZeAsxxBVba9ekoZmbW0tl8bTw4F7W8_4c759Sr5kG1iNFuj414KB2JHhc5sCqMwOhUfYbkWiLU3ECWPglPOD0JBdZM5utjfRioozZLIdE-Icl3teUk65KUwQyl8HA94f9HGYVOuj4eIwRhwkxCFxAQhCuZLdwAAAAHx6C_MAA"
